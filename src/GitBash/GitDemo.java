cdb97583b3b3259b3471eb42085ee9922994ddcf
allendaniel05 <allendaniel7.ad@gmail.com>
2023-06-15 12:52:47 +0530

    Commit Message

---
+++ src/GitBash/GitDemo.java
@@ -0,0 +1,9 @@
+package GitBash;
+
+public class GitDemo {
+public static void main(String[] args) {
+	System.out.println("Comit1");
+	System.out.println("Comit2");
+	System.out.println("Comit3");
+}
+}
